5a20e75f6faf4b6f9f84ceafcde27ad917eed365
Gabriel Vale <gogvale2011@gmail.com>
2020-05-09 14:17:46 -0500

    Separate header and footer

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
-<!DOCTYPE html>
-<html lang="es">
+<?php include 'include/header.php' ?>
+<h1>Build a cake</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat labore, aperiam
+    expedita tempora ducimus ab ipsam non neque libero commodi nulla quis tenetur
+    placeat repellat, dolor error nesciunt, modi quam perspiciatis repellendus at.
+    Aliquid hic ullam quaerat, cumque mollitia at necessitatibus sequi dolor laborum
+    quasi reiciendis, nesciunt delectus tempora corporis pariatur iste, repellat quod
+    possimus laudantium in molestiae distinctio nemo nihil? Necessitatibus, id eligendi
+    sed dolorem animi ex itaque dolore, optio rerum temporibus at officiis maxime non,
+    in eum hic sunt corporis harum mollitia incidunt voluptatibus neque laboriosam.
+    Ipsam amet eius voluptatum enim provident. Accusantium consequuntur perspiciatis
+    vitae laudantium aliquam!</p>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <title>Build-a-Cake! - Carioca Lanches®</title>
-</head>
-
-<body>
-    <h1>Build a cake</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat labore, aperiam expedita tempora ducimus ab ipsam non neque libero commodi nulla quis tenetur placeat repellat, dolor error nesciunt, modi quam perspiciatis repellendus at. Aliquid hic ullam quaerat, cumque mollitia at necessitatibus sequi dolor laborum quasi reiciendis, nesciunt delectus tempora corporis pariatur iste, repellat quod possimus laudantium in molestiae distinctio nemo nihil? Necessitatibus, id eligendi sed dolorem animi ex itaque dolore, optio rerum temporibus at officiis maxime non, in eum hic sunt corporis harum mollitia incidunt voluptatibus neque laboriosam. Ipsam amet eius voluptatum enim provident. Accusantium consequuntur perspiciatis vitae laudantium aliquam!</p>
-</body>
-
-</html>
+<?php include 'include/footer.php' ?>
