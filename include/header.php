41b841c7cc21cd2a04946dee2f6ff9d9dfbd8bb4
Gabriel Vale <gogvale2011@gmail.com>
2020-06-22 16:12:24 -0500

    First official header

--- include/header.php
+++ include/header.php
@@ -6,6 +6,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <title> <?= $title ?? "Build-a-Cake!" ?> - Carioca Lanches®</title>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="CSS/reset.css" />
+    <link rel="stylesheet" href="CSS/header.css" />
 </head>
 
 <body>
+    <header>
+        <div class="pink banner">
+            <h2>Build a Cake</h2>
+            <a href="#">Idioma</a>
+        </div>
+        <div class="yellow banner">
+            <nav>
+                <a href="#">Inicio</a>
+                <a href="#">Nosotros</a>
+                <a href="#">Galería</a>
+                <a href="#">Build It!</a>
+            </nav>
+            <div class="buttons">
+                <a href="#">
+                    <figure>
+                        <img src="./IMG/greencake.svg" alt="Green Cake" />
+                        <figcaption>
+                            Registarse
+                        </figcaption>
+                    </figure>
+                </a>
+
+                <a href="#">
+                    <figure>
+                        <img src="./IMG/pinkcake.svg" alt="Pink Cake" />
+                        <figcaption>
+                            Inicio de Sesión
+                        </figcaption>
+                    </figure>
+                </a>
+            </div>
+        </div>
+    </header>
--- index.php
+++ index.php
@@ -1,33 +1,7 @@
 <?php
 // $title = "Lorem Ipsum";
-require 'include/header.php';
+require_once 'include/header.php';
 ?>
-<style>
-    div {
-        display: inline-flex;
-        border: 1px black solid;
-        text-align: center;
-    }
-
-    a {
-        text-decoration: none;
-    }
-</style>
-<h1>Build a cake</h1>
-<p>Seleccionar opción:</p>
-<br>
-<div>
-    <a href="#">
-        <img name="cake1" class="card" title="catalogo" src="IMG/Cake.Canudo.svg">
-        <br> Catálogo
-    </a>
-</div>
-<div>
-    <a href="build-cake.php">
-        <img class="card" title="construir tu pastel" src="IMG/Cake.camadas.svg">
-        <br> Crear Pastel
-    </a>
-</div>
 
 
-<?php require 'include/footer.php' ?>
+<?php require_once 'include/footer.php' ?>
