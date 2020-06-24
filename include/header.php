45406ed9402c4bd2d50b2b6205cf447c748a930a
Gabriel Vale <gogvale2011@gmail.com>
2020-06-24 10:33:27 -0500

    correct home path and hide register

--- include/header.php
+++ include/header.php
@@ -22,12 +22,12 @@
         </div>
         <div class="yellow banner">
             <nav>
-                <a <?php if ($fileName == '/index.php') echo 'class="active"'; ?> href="">Inicio</a>
+                <a <?php if ($fileName == '/index.php') echo 'class="active"'; ?> href="/">Inicio</a>
                 <a <?php if ($fileName == '/nosotros.php') echo 'class="active"'; ?> href="nosotros.php">Nosotros</a>
                 <a <?php if ($fileName == '/galeria.php') echo 'class="active"'; ?> href="galeria.php">Galería</a>
                 <a <?php if ($fileName == '/build-it.php') echo 'class="active"'; ?> href="build-it.php">Build It!</a>
             </nav>
-            <div class="buttons">
+            <div style="visibility:hidden" class="buttons">
                 <a href="#">
                     <figure>
                         <img src="./IMG/greencake.svg" alt="Green Cake" />
