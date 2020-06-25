b3a661a49937c95d6f466b6c41f4006903069ab8
Gabriel Vale <gogvale2011@gmail.com>
2020-06-25 09:20:05 -0500

    Add "nosotros" content

--- nosotros.php
+++ nosotros.php
@@ -2,4 +2,14 @@
 // $title = "Lorem Ipsum";
 include 'include/header.php';
 ?>
+<link rel="stylesheet" href="CSS/content.css">
+<div class="container">
+    <h3>História</h3>
+    <p>O fuxico é uma técnica artesanal em que há o reaproveitamento de retalhos de tecido. O retalho é recortado em formato circular e alinhava-se suas extremidades. Ao puxar a linha, forma-se uma trouxinha de tecido, aí está formado o fuxico. Na elaboração desta técnica são normalmente utilizados: tesoura, retalhos de tecidos, agulhas, linhas, um molde de formato redondo. Ele é composto especialmente com a união de várias pequenas trouxas de tecido, as quais, entre-tecidas, constituem flores coloridas. Estas, ao se conectarem, dão um novo visual a confecções, bolsas, tapetes, colares, broches, colchas, entre outros itens.
+    </p>
+    <p>No Brasil, a história do fuxico tem início no período colonial. Eles foram criados para que existisse o aproveitamento de sobras de tecidos, que eram artigos de luxo na época. O fuxico sempre esteve associado à comunidades de baixa renda, mas com a introdução de diversas técnicas artesanais na moda e na decoração, ele começou a ser mais valorizado e utilizado.
+    </p>
+    <p>A expressão que dá nome a este artesanato provém do interior da própria cultura popular. Na região nordeste do Brasil as mulheres se uniam para confeccionar e, enquanto isso, não perdiam tempo, colocavam fofocas e mexericos em dia, ou seja, realizavam seus famosos fuxicos. Assim nasceu o termo que batizaria esta modalidade artesanal.
+    </p>
+</div>
 <?php include 'include/footer.php' ?>
