7812e8ad31a45c7ae7e3ba88d24a36dff0061cc1
Gabriel Vale <gogvale2011@gmail.com>
2020-06-25 14:09:51 -0500

    Change nav language

--- en/nav.php
+++ en/nav.php
@@ -1,8 +1,8 @@
 <div class="yellow banner">
     <nav>
-        <a <?php if ($_SERVER['PHP_SELF'] == '/en/index.php') echo 'class="active"'; ?> href="index.php">Inicio</a>
-        <a <?php if ($_SERVER['PHP_SELF'] == '/en/nosotros.php') echo 'class="active"'; ?> href="nosotros.php">Nosotros</a>
-        <a <?php if ($_SERVER['PHP_SELF'] == '/en/galeria.php') echo 'class="active"'; ?> href="galeria.php">Galería</a>
+        <a <?php if ($_SERVER['PHP_SELF'] == '/en/index.php') echo 'class="active"'; ?> href="index.php">Start</a>
+        <a <?php if ($_SERVER['PHP_SELF'] == '/en/nosotros.php') echo 'class="active"'; ?> href="nosotros.php">About Us</a>
+        <a <?php if ($_SERVER['PHP_SELF'] == '/en/galeria.php') echo 'class="active"'; ?> href="galeria.php">Galery</a>
         <!-- <a <?php if ($_SERVER['PHP_SELF'] == '/en/build-it.php') echo 'class="active"'; ?> href="build-it.php">Build It!</a> -->
     </nav>
     <div style="visibility:hidden" class="buttons">
@@ -10,7 +10,7 @@
             <figure>
                 <img src="/IMG/greencake.svg" alt="Green Cake" />
                 <figcaption>
-                    Registarse
+                    Sign Up
                 </figcaption>
             </figure>
         </a>
@@ -19,7 +19,7 @@
             <figure>
                 <img src="/IMG/pinkcake.svg" alt="Pink Cake" />
                 <figcaption>
-                    Inicio de Sesión
+                    Sign In
                 </figcaption>
             </figure>
         </a>
--- pt-br/nav.php
+++ pt-br/nav.php
@@ -1,8 +1,8 @@
 <div class="yellow banner">
     <nav>
         <a <?php if ($_SERVER['PHP_SELF'] == '/pt-br/index.php') echo 'class="active"'; ?> href="index.php">Inicio</a>
-        <a <?php if ($_SERVER['PHP_SELF'] == '/pt-br/nosotros.php') echo 'class="active"'; ?> href="nosotros.php">Nosotros</a>
-        <a <?php if ($_SERVER['PHP_SELF'] == '/pt-br/galeria.php') echo 'class="active"'; ?> href="galeria.php">Galería</a>
+        <a <?php if ($_SERVER['PHP_SELF'] == '/pt-br/nosotros.php') echo 'class="active"'; ?> href="nosotros.php">Sobre Nós</a>
+        <a <?php if ($_SERVER['PHP_SELF'] == '/pt-br/galeria.php') echo 'class="active"'; ?> href="galeria.php">Galeria</a>
         <!-- <a <?php if ($_SERVER['PHP_SELF'] == '/pt-br/build-it.php') echo 'class="active"'; ?> href="build-it.php">Build It!</a> -->
     </nav>
     <div style="visibility:hidden" class="buttons">
@@ -10,7 +10,7 @@
             <figure>
                 <img src="/IMG/greencake.svg" alt="Green Cake" />
                 <figcaption>
-                    Registarse
+                    Registro
                 </figcaption>
             </figure>
         </a>
@@ -19,7 +19,7 @@
             <figure>
                 <img src="/IMG/pinkcake.svg" alt="Pink Cake" />
                 <figcaption>
-                    Inicio de Sesión
+                    Login
                 </figcaption>
             </figure>
         </a>
