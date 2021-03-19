d91653c481d170d5a86ad87d139dc2564654b398
Gabriel Vale <gogvale2011@gmail.com>
2021-03-19 11:05:01 -0600

    Comments on script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $1
 
-find . -name '*.html' -exec sed -i 's/\.html/\.php/g' "{}" \;
-find . -name '*.xml' -exec sed -i 's/\.html/\.php/g' "{}" \;
-find . -depth -name "*.html" -exec sh -c 'mv "$1" "${1%.html}.php"' _ {} \;
+find . -name '*.html' -exec sed -i 's/\.html/\.php/g' "{}" \;                   # Replace nav links with PHP
+sed -i 's/\.html/\.php/g' ./sitemap.xml                                         # Update sitemap with PHP
+find . -depth -name "*.html" -exec sh -c 'mv "$1" "${1%.html}.php"' _ {} \;     # Replace files with PHP
