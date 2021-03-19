6d7b88b0dc6b71be43430ef85de545041a7bfd4e
Gabriel Vale <gogvale2011@gmail.com>
2021-03-19 12:23:18 -0600

    keep html for gh-pages

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd $1
 
-find . -name '*.html' -exec sed -i 's/\.html/\.php/g' "{}" \;                   # Replace nav links with PHP
+cp *.html docs
+find . -maxdepth 1 -name "*.html" -exec sed -i 's/\.html/\.php/g' "{}" \;                   # Replace nav links with PHP
 sed -i 's/\.html/\.php/g' ./sitemap.xml                                         # Update sitemap with PHP
-find . -depth -name "*.html" -exec sh -c 'mv "$1" "${1%.html}.php"' _ {} \;     # Replace files with PHP
+find . -maxdepth 1 -name "*.html" -exec sh -c 'mv "$1" "${1%.html}.php"' _ {} \;     # Replace files with PHP
