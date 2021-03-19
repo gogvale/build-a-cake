88ea47bff837a24a8896a7882b4f357c612ff1de
Gabriel Vale <gogvale2011@gmail.com>
2021-03-19 12:30:28 -0600

    add resources gh-pages

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd $1
 
-cp *.html docs
+cp -r *.html assets sitemap.xml docs
 find . -maxdepth 1 -name "*.html" -exec sed -i 's/\.html/\.php/g' "{}" \;                   # Replace nav links with PHP
 sed -i 's/\.html/\.php/g' ./sitemap.xml                                         # Update sitemap with PHP
 find . -maxdepth 1 -name "*.html" -exec sh -c 'mv "$1" "${1%.html}.php"' _ {} \;     # Replace files with PHP
